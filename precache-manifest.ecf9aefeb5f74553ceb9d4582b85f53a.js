550e2c37f33397e43fb155135b4602af39313aca
Dario Lostumbo <dario.lostumbo@hotmail.com>
2019-09-07 12:48:05 +0200

    Updates

---
+++ precache-manifest.ecf9aefeb5f74553ceb9d4582b85f53a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1236577b2336248c5fd7472772677685",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e8090869100be947555c",
+    "url": "/monsters-rolodex/static/css/main.3e410802.chunk.css"
+  },
+  {
+    "revision": "4ec9dbe61cc08490ea57",
+    "url": "/monsters-rolodex/static/js/2.21bccb0f.chunk.js"
+  },
+  {
+    "revision": "e8090869100be947555c",
+    "url": "/monsters-rolodex/static/js/main.9aa9bb28.chunk.js"
+  },
+  {
+    "revision": "39237e6c65c9f8723ed2",
+    "url": "/monsters-rolodex/static/js/runtime~main.bc49cb6f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
